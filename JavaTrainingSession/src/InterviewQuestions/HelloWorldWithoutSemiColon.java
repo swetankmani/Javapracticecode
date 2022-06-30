bd4c692da3c17d5a5cf08979f2e72c66f78b1272
ASK <ASK@ASK-PC>
2022-06-30 17:51:23 +0530

    First Interview Questions

---
+++ JavaTrainingSession/src/InterviewQuestions/HelloWorldWithoutSemiColon.java
@@ -0,0 +1,15 @@
+package InterviewQuestions;
+
+
+public class HelloWorldWithoutSemiColon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if(System.out.printf("Hello World")==null) {
+
+	}
+
+}
+	
+}
