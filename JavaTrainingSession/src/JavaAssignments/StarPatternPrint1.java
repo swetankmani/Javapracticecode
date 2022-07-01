7b11985ed4183e6943894e5b488ced5e6356203d
ASK <ASK@ASK-PC>
2022-07-01 15:51:45 +0530

    Star print

---
+++ JavaTrainingSession/src/JavaAssignments/StarPatternPrint1.java
@@ -0,0 +1,20 @@
+package JavaAssignments;
+
+public class StarPatternPrint1 {
+
+	public static void main(String[] args) {
+		
+
+		for(int x=0;x<=4;x++ ) {
+			for(int j=4;j>=x;j--) {
+				
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			
+			
+		}
+	}
+
+}
---
+++ JavaTrainingSession/src/JavaAssignments/StarPatternTrianglePrint.java
@@ -0,0 +1,24 @@
+package JavaAssignments;
+
+public class StarPatternTrianglePrint {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=4;i++) {
+			for(int j=0;j<=i;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int x=0;x<=4;x++) {
+			for(int y=4;y>=x;y--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
